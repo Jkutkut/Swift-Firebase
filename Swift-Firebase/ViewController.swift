1aead5fddec4f4cc17a986c5f705246930539db7
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-03-01 09:11:15 +0100

    UI created

---
+++ Swift-Firebase/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Swift-Firebase
+//
+//  Created by Usuario invitado on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtfExpediente: UITextField!
+    @IBOutlet weak var txtfNombre: UITextField!
+    @IBOutlet weak var txtfEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
