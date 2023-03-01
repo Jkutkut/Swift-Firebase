a79ad607aeafcdb2e6979fd0aac8e3330a5feea7
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-03-01 11:42:46 +0100

    able to add, get and delete elements in firebase

--- Swift-Firebase/ViewController.swift
+++ Swift-Firebase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
@@ -13,11 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtfNombre: UITextField!
     @IBOutlet weak var txtfEmail: UITextField!
     
+    private let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onBtnAlmacenar(_ sender: Any) {
+        let key: String = txtfExpediente.text!
+        let user: String = txtfNombre.text ?? ""
+        let email: String = txtfEmail.text ?? ""
 
+        db.collection("usuarios")
+            .document(key)
+            .setData([
+                "usuario": user,
+                "email": email
+            ])
+    }
+    
+    @IBAction func onBtnRecuperar(_ sender: Any) {
+        let key: String = txtfExpediente.text!
+        db.collection("usuarios").document(key)
+            .getDocument{ (document: DocumentSnapshot?, e: Error?) in
+                self.txtfNombre.text = document?.get("usuario") as? String
+                self.txtfEmail.text = document?.get("email") as? String
+            }
+    }
+
+    @IBAction func onBtnEliminar(_ sender: Any) {
+        let key: String = txtfExpediente.text!
+        db.collection("usuarios").document(key)
+            .delete()
+    }
 }
 
